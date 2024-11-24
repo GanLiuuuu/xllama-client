deaaf3d2597e7177764cc2f52bf25a233cc2f5b1
GanLiuuuu <119108256+GanLiuuuu@users.noreply.github.com>
2024-11-24 20:21:45 +0800

    Feature: user can upload photo in chat

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="chat-area">
-      <div style="overflow: auto;height: 700px;" v-html="content"></div>
+      <div style="overflow: auto;height: 550px;" v-html="content"></div>
+      <div class="bg-gray-50 sm:rounded-lg" style="text-align: left;">
+  <div class="px-4 py-5 sm:p-6">
+    <h3 class="text-base font-semibold text-gray-900">Multiple Query Formats?</h3>
+    <div class="mt-2 max-w-xl text-sm text-gray-500">
+      <p>You can ask questions via text, images, files, and voice inputs.</p>
+    </div>
+    <div class="mt-5">
+      <button type="button" class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Upload photo</button>
+    </div>
+  </div>
+</div>
       <div class="mt-5 sm:flex sm:items-center">
         <input type="text" @keyup.enter="sendMsg" v-model="text"  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" placeholder="Message XLlama" />
         <button @click="sendTextMessage" class="mt-3 inline-flex w-full items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:ml-3 sm:mt-0 sm:w-auto">Send</button>
