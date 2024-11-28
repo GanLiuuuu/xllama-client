0537f2e92063616c69525a36ac695dde33a8e4c8
GanLiuuuu <119108256+GanLiuuuu@users.noreply.github.com>
2024-11-28 19:48:19 +0800

    Feature: add selece bot module

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -11,6 +11,32 @@
           <input type="file" @change="handleFileUpload" style="display: none" ref="fileInput" />
           <button @click="triggerFileInput" type="button" class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Upload photo</button>
         </div>
+        
+        <Listbox as="div" v-model="selected">
+          <ListboxLabel class="block text-sm/6 font-medium text-gray-900">Assigned to</ListboxLabel>
+          <div class="relative mt-2">
+            <ListboxButton class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm/6">
+              <span class="block truncate">{{ selected.name }}</span>
+              <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                <ChevronUpDownIcon class="size-5 text-gray-400" aria-hidden="true" />
+              </span>
+            </ListboxButton>
+
+            <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
+              <ListboxOptions class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                <ListboxOption as="template" v-for="person in people" :key="person.id" :value="person" v-slot="{ active, selected }">
+                  <li :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9']">
+                    <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">{{ person.name }}</span>
+
+                    <span v-if="selected" :class="[active ? 'text-white' : 'text-indigo-600', 'absolute inset-y-0 right-0 flex items-center pr-4']">
+                      <CheckIcon class="size-5" aria-hidden="true" />
+                    </span>
+                  </li>
+                </ListboxOption>
+              </ListboxOptions>
+            </transition>
+          </div>
+        </Listbox>
       </div>
     </div>
     <div class="mt-5 sm:flex sm:items-center">
@@ -19,9 +45,27 @@
     </div>
   </div>
 </template>
+<script setup>
+import { ref } from 'vue'
+import { Listbox, ListboxButton, ListboxLabel, ListboxOption, ListboxOptions } from '@headlessui/vue'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
+
+const people = [
+  { id: 1, name: 'Claude' },
+  { id: 2, name: 'GPT3.5' },
+  { id: 3, name: 'GPT4' },
+  { id: 4, name: 'Gemini' }
+  
+]
+
+const selected = ref(people[3])
+</script>
 
 <script>
+import { Listbox, ListboxButton, ListboxLabel, ListboxOption, ListboxOptions } from '@headlessui/vue'
 import { OpenAI } from "openai";
+import { ref } from 'vue'
+
 export default {
   data() {
     return {
@@ -40,6 +84,9 @@ export default {
         basePath: "https://api.openai-proxy.org/v1",
         dangerouslyAllowBrowser: true,
       }),
+      
+     
+      
     };
   },
   mounted() {
